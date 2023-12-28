cd2ca11cef6b0d2b5c138b272b2f0506daa11458
Rodrigo Grzegorczyk <rodrigo.gregorio1@hotmail.com>
2023-12-28 20:40:09 -0300

    profile

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,19 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      'blue': '#3294F8',
+      'base-title': '#E7EDF4',
+      'base-subtitle': '#C4D4E3',
+      'base-text': '#AFC2D4',
+      'base-span': '#7B96B2',
+      'base-label': '#3A536B',
+      'base-border': '#1C2F41',
+      'base-post': '#112131',
+      'base-profile': '#0B1B2B',
+      'base-background': '#071422',
+      'base-input': '#040F1A ',
+      'white': '#FFF',
     },
   },
   plugins: [],
